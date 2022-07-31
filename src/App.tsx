0204431cd611bfedbf4ad2d220ca47080f17c6a7
towa1204 <lsadsfj@gmail.com>
2022-07-31 17:03:22 +0900

    見た目をcssファイルに移動

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,9 @@ const App = () => {
   };
 
   return (
-    <div className="App" style={{ height: '100%' }}>
+    <div className="App">
       <h1>Scroll Light Viewer</h1>
+      <h3>画像・動画ファイルを縦スクロールして閲覧できます</h3>
       <InputFilesArea appendFiles={appendFiles}>
         <ViewFiles files={files} />
       </InputFilesArea>
@@ -51,15 +52,9 @@ const InputFilesArea = ({
   });
 
   const borderNormalStyle = {
-    width: '90%',
-    height: '100%',
-    margin: '0 auto',
     border: '1px dotted #888',
   };
   const borderDragStyle = {
-    width: '90%',
-    height: '100%',
-    margin: '0 auto',
     border: '1px solid #00f',
     transition: 'border .5s ease-in-out',
     backgroundColor: '#2e2e2e',
@@ -67,44 +62,35 @@ const InputFilesArea = ({
   };
 
   return (
-    <div {...getRootProps()} style={isDragActive ? borderDragStyle : borderNormalStyle}>
+    <div {...getRootProps()} id="dragarea" style={isDragActive ? borderDragStyle : borderNormalStyle}>
       <input {...getInputProps()} />
-      {isDragActive ? <p>ここにドラッグ&ドロップ</p> : <p>画像・動画ファイルをドロップ</p>}
-      <button type="button" onClick={open}>
+      <button className="c-button" type="button" onClick={open}>
         画像・動画ファイルを選択
       </button>
+      {isDragActive ? <p>ここにドラッグ&ドロップ</p> : <p>または画像・動画ファイルをドロップ</p>}
       {children}
     </div>
   );
 };
 
 const ViewFiles = ({ files }: { files: Array<MediaFile> }) => {
-  const viewStyle = {
-    maxWidth: '100%',
-    maxHeight: '100vh',
-  };
-
   const listFiles = files.map((file) => {
     if (file.type === 'video/mp4') {
       // file.nameをkeyにするのはあまりよくない気がしてる
       return (
         <div key={file.name}>
-          <video style={viewStyle} src={file.preview} controls />
+          <video src={file.preview} controls />
         </div>
       );
     }
     return (
       <div key={file.name}>
-        <img style={viewStyle} src={file.preview} />
+        <img src={file.preview} />
       </div>
     );
   });
 
-  return (
-    <div id="viewer" style={{ width: '80%', margin: '0 auto' }}>
-      {listFiles}
-    </div>
-  );
+  return <div id="viewer">{listFiles}</div>;
 };
 
 export default App;
